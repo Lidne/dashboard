cc8ddce782b01794d05866014b2f1b659fcf5e76
rusna <rusnakkaleksandr@gmail.com>
2024-04-17 21:48:18 +0300

    parsing news

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,15 +1,16 @@
 from typing import Union
-
 from fastapi import FastAPI
+from parsing_news import news_list
 
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    return {"name": "astra"}
+@app.get("/news")
+async def new_list(from_: str, to_: str = "") -> dict:
+    dict_news = {"NEWS": news_list(from_, to_)}
+    return dict_news
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: Union[int, None] = None, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
